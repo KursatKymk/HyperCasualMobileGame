761afcd35122d541d8641d455c576e36944f628f
Kürşat <kursatkiymakk@gmail.com>
2024-06-17 17:11:20 +0300

    asd

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
